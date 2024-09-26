8a5cb8d35ccbe31bdb13991f64437e6ded314966
vijayvora079 <vijay.patel.it09@gmail.com>
2024-09-26 14:20:08 +0530

    url makes more meaningful

--- src/main/java/com/springboot/demo/DemoController.java
+++ src/main/java/com/springboot/demo/DemoController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @RequestMapping(method = RequestMethod.GET, path = "/")
+    @RequestMapping(method = RequestMethod.GET, path = "/hello")
     public String hello(){
         return "I heard it!";
     }
