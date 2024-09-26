a5413b42445ed64265f4a9849e89c9fbd1fdcafe
vijayvora079 <vijay.patel.it09@gmail.com>
2024-09-26 14:16:51 +0530

    Add files via upload

---
+++ src/main/java/com/springboot/demo/DemoController.java
@@ -0,0 +1,14 @@
+package com.springboot.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    public String hello(){
+        return "I heard it!";
+    }
+}
